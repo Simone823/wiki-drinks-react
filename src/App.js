0d86aae46f97300b26d688d8dec5a320c59dba8e
Simone823 <simone.daglio823@outlook.it>
2022-11-04 09:48:36 +0100

    import header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 // import react router 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import pags home
-import Home from './pages/Home';
-
 // import useAppGlobalContext custom hook
 import { useAppGlobalContext } from './context';
+
+// import Header
+import Header from './components/Header';
+
+// import pags home
+import Home from './pages/Home';
 
 // import footer
 import Footer from './components/Footer';
@@ -18,6 +21,8 @@ function App() {
 
   return (
     <Router className="App">
+      {/* header */}
+      <Header/>
 
       {/* routes */}
       <Routes>
