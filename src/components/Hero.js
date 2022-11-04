fc688aceeef8d5323aad258d52611efdb0eaef22
Simone823 <simone.daglio823@outlook.it>
2022-11-04 20:04:01 +0100

    hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/css/hero.css';
+
+function Hero({children, img, isOverlay}) {
+    console.log(img)
+
+  return (
+    <div id='hero' style={
+            {
+                backgroundImage: `url(${img})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+            }
+        }
+    >
+        <div className={`h-full ${isOverlay ? 'hero-overlay' : ''}`}>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+// import hero component
+import Hero from '../components/Hero';
+
+// import hero image home
+import heroHome from '../assets/image/home-hero.jpg';
 
 function Home() {
   return (
-    <div>Home</div>
+    <section id='home'>
+      {/* hero */}
+      <Hero img={heroHome} isOverlay={true}>
+        <h1>hero text</h1>
+      </Hero>
+    </section>
   )
 }
 
-export default Home
+export default Home;
