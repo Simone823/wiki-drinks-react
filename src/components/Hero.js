7ab331b83e4aaf6ca6e4448bcfab9c0a6c2d719f
Simone823 <simone.daglio823@outlook.it>
2022-11-05 21:27:42 +0100

    modifica hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,17 +5,19 @@ function Hero({children, img, isOverlay}) {
 
   return (
     <div id='hero' style={
-            {
-                backgroundImage: `url(${img})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-            }
+        {
+          backgroundImage: `url(${img})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
         }
+      }
     >
-        <div className={`h-full ${isOverlay ? 'hero-overlay' : ''}`}>
-            {children}
-        </div>
+
+      {/* hero text */}
+      <div className={`h-full px-3 ${isOverlay ? 'hero-overlay' : ''}`}>
+        {children}
+      </div>
     </div>
   )
 }
