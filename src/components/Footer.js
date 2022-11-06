784b9b799e36847093585001ad17f5e4f14d8f6a
Simone823 <simone.daglio823@outlook.it>
2022-11-06 02:18:42 +0100

    refactoring footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
     <footer id='footer' className='bg-violet-900 text-white'>
-        <div className='container mx-auto py-4 px-3 text-center'>
-            <h3 className='text-sm uppercase font-bold'>All &copy; reserved to <span className='text-pink-500'>drink team</span></h3>
-        </div>
+      <div className='container mx-auto py-4 px-3 text-center'>
+        <h3 className='text-sm uppercase font-bold'>All &copy; reserved to <span className='text-pink-500'>drink team</span></h3>
+      </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
