2d8a11e0ecc933c1b11884f5fedce7c32bf6443c
Simone823 <simone.daglio823@outlook.it>
2022-11-08 18:32:24 +0100

    cocktail card component

---
+++ src/components/Cocktail.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../assets/css/components/cocktail.css';
+import {IoMdPaper} from 'react-icons/io';
+
+import { Link } from 'react-router-dom';
+
+
+function Cocktail({id, name, image}) {
+  return (
+    <div className='wrapper-cocktail w-100 h-72'>
+      {/* img */}
+      <div className='img-cocktail max-w-100 h-full'>
+        <img className='object-cover object-center' src={image} alt={name}/>
+      </div>
+
+      {/* hover container */}
+      <div className='text text-white w-full py-4 px-3 bg-black/80 flex justify-between items-center flex-wrap'>
+        <h6 className='font-bold text-sm'>{name}</h6>
+        <Link to={`/cocktail/${id}`} className='font-bold text-xs text-pink-500 flex gap-2 hover:text-violet-500 duration-300 max-w-max'>
+          Ricetta
+          <IoMdPaper />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Cocktail;
