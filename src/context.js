7729be87e7fb4ed49db2006d125e0d6f4c5c056a
Simone823 <simone.daglio823@outlook.it>
2022-11-08 15:35:15 +0100

    usefetch context hook

--- src/context.js
+++ src/context.js
@@ -1,12 +1,36 @@
 import { useState, useContext, createContext } from "react";
 
+// useFetch custom hook
+import useFetch from "./customHook/useFetch";
+
 // App context
 const AppContext = createContext();
 
 // AppProvider component
 const AppProvider = ({children}) => {
+
+    // query serach cocktail
+    const [queryCokctail, setQueryCocktail] = useState('gin');
+
+    // is loading, is error, data, count
+    const {isLoading, isError, data, count} = useFetch(`s=${queryCokctail}`, true);
+
+    // search cocktail
+    function searchCocktail (input) {
+        setQueryCocktail(input);
+    }
+
     return (
-        <AppContext.Provider value="test app context">
+        <AppContext.Provider value={
+            {
+                queryCokctail,
+                isLoading,
+                isError,
+                data,
+                count,
+                searchCocktail
+            }
+        }>
             {children}
         </AppContext.Provider>
     );
