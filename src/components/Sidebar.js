31949598a3b6a4c9beda0bd2b765264bd583e6fa
Simone823 <simone.daglio823@outlook.it>
2022-11-09 15:32:28 +0100

    mb link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,17 +26,17 @@ function Sidebar() {
                 </div>
 
                 {/* list links */}
-                <ul className='list-links mb-5'>
-                    <li>
-                        <Link onClick={closeMenu()} to="/" className='nav-link font-bold uppercase text-sm flex items-center gap-x-4 mb-5'>
+                <ul className='list-links mb-10'>
+                    <li className='mb-10'>
+                        <Link onClick={closeMenu()} to="/" className='nav-link font-bold uppercase text-sm flex items-center gap-x-4'>
                             <svg stroke="currentColor" fill="currentColor" viewBox="0 0 1024 1024" className="nav-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M946.5 505L534.6 93.4a31.93 31.93 0 0 0-45.2 0L77.5 505c-12 12-18.8 28.3-18.8 45.3 0 35.3 28.7 64 64 64h43.4V908c0 17.7 14.3 32 32 32H448V716h112v224h265.9c17.7 0 32-14.3 32-32V614.3h43.4c17 0 33.3-6.7 45.3-18.8 24.9-25 24.9-65.5-.1-90.5z"></path>
                             </svg>
                             Home
                         </Link>
                     </li>
-                    <li>
-                        <Link onClick={closeMenu()} to="/about" className='nav-link font-bold uppercase text-sm flex items-center gap-x-4 mb-5'>
+                    <li className='mb-10'>
+                        <Link onClick={closeMenu()} to="/about" className='nav-link font-bold uppercase text-sm flex items-center gap-x-4'>
                             <svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" className="nav-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                 <g>
                                     <path fill="none" d="M0 0h24v24H0z"></path><path d="M12 11a5 5 0 0 1 5 5v6H7v-6a5 5 0 0 1 5-5zm-6.712 3.006a6.983 6.983 0 0 0-.28 1.65L5 16v6H2v-4.5a3.5 3.5 0 0 1 3.119-3.48l.17-.014zm13.424 0A3.501 3.501 0 0 1 22 17.5V22h-3v-6c0-.693-.1-1.362-.288-1.994zM5.5 8a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zm13 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zM12 2a4 4 0 1 1 0 8 4 4 0 0 1 0-8z">
